1be223d69a153d8562f744066eb11f8f462d34ce
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-08-30 13:15:56 -0300

    feat: added typeorm

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(csurf({ cookie: true }));
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Server running at http://localhost:${process.env.PORT || 3000}`);
 }
+
 bootstrap();
